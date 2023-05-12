45fb8971db6198559d0853b7160a1d324c187207
yoda <98830917+ydt-yash-1738@users.noreply.github.com>
2023-05-12 15:05:12 +0530

    fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -23,28 +23,18 @@
     
 //     return 0;
 // }
+//0 1 1 2 3 5 8 13 21
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
-    if(n==0) return 0;
-    if (n==1)
-    cout<<"0 1";
-    int fib[n+1];
-    fib[0]=0;
-    fib[1]=1;
-    for (int i = 2; i <= n; i++)
-    {
-        fib[i]=fib[i-1]+fib[i-2];
-    }
-    
-
-    cout<<endl;
-    
-    
-    return 0;
+        int a=0, b=1, c;
+        for(int i=0; i<n-1; i++){
+                c=a+b;
+                a=b;
+                b=c;
+        }
+        return c; //will return the number of corresponding index
 }
 //0 1 1 2 3 5 8 13 21
