6dd36a4ee7a293ec1e9024b1da34e2e932195aa5
Yashdev Tiwari <yashdt50@gmail.com>
2024-04-01 20:30:09 +0530

    Add files via upload

---
+++ heaps.cpp
@@ -0,0 +1,149 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class heap{
+    public:
+    int arr[100];
+    int size = 0;
+
+    heap(){
+        arr[0] = -1;
+        size = 0;
+    }
+//TC: O(log n)
+    void insert(int val){
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+        
+        while (index > 1){
+            int parent = index/2;
+            if(arr[parent] < arr[index]){
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else{
+                return;
+            }
+        }
+    }
+//TC: O(log n)
+    void deleteNode(int val){
+        if(size == 0){
+            cout<< "Nothing to delete" <<endl;
+            return;
+        }
+        //put last element into first index
+        arr[1] = arr[size];
+
+        //remove last element
+        size--;
+
+        //take root node to its correct position
+        int i = 1;
+        while(i < size){
+            int leftIndex = 2*i;
+            int rightIndex = 2*i + 1;
+
+            if(leftIndex , size && arr[i] < arr[leftIndex]){
+                swap(arr[i], arr[leftIndex]);
+                i = leftIndex;
+            }else if(rightIndex <= size && arr[i] < arr[rightIndex]){
+                swap(arr[i], arr[rightIndex]);
+                i = rightIndex;
+            }else{
+                return;
+            }
+        }
+    }
+
+
+    void print(){
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }cout<<endl;
+    }
+};
+
+//TC: O(log n)
+void heapify(int arr[], int n, int i){
+        int largest = i;
+        int left = 2*i;
+        int right = 2*i + 1;
+
+        if(left <= n && arr[largest] < arr[left]){
+            largest = left;
+        }
+        if(right <= n && arr[largest] < arr[right]){
+            largest = right;
+        }
+        if(largest != i){
+            swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
+    }
+
+//TC: O(nlog n)
+void heapSort(int arr[], int n){
+    
+    int size = n;
+
+    while(size > 1){
+        swap(arr[size], arr[1]); //step 1
+        size--; //step 2
+
+        heapify(arr, size, 1); //step 3
+    }
+}
+
+
+
+
+int main(){
+    heap h;
+    h.insert(50);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
+    h.print();
+
+    h.deleteNode(52);
+    h.print();
+
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
+
+    //TC: O(log n)
+    for(int i=n/2; i>0; i--){
+        heapify(arr, n, i);
+    }
+    cout<<"Printing the array: "<<endl;
+    for(int i=1; i<=n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    //heapSort
+    heapSort(arr, n);
+    cout<<"Printing the sorted array: "<<endl;
+    for(int i=1; i<=n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl; 
+
+
+    cout<<"Priority queue"<<endl;
+    priority_queue<int> pq;
+    pq.push(4);
+    pq.push(2);
+    pq.push(5);
+    pq.push(3);
+
+    cout<<"Top element"<< pq.top() << endl;
+    pq.pop();
+    cout<<"Top element after pop()"<< pq.top() << endl;
+    cout<< "Size is" << pq.size() <<endl;
+
+    return 0;
+}
